857cea32dabe75e1c0df1879a474ffb0d72be9b5
WozHuang <wozhuang96@gmail.com>
2024-08-17 17:12:18 +0800

    rxjs

--- web/rxjs/main.ts
+++ web/rxjs/main.ts
@@ -1,6 +1,69 @@
-import { from, fromEvent, map, of, Subject } from 'rxjs';
+import {
+  BehaviorSubject,
+  from,
+  fromEvent,
+  map,
+  Observable,
+  Subscription,
+  of,
+  ReplaySubject,
+  share,
+  Subject,
+  mergeMap, skip
+} from 'rxjs';
 
-fromEvent(document, 'click').subscribe(() => console.log('Clicked!'));
+let behaviorSubject = new BehaviorSubject<string | null>(null);
+const request = () => {
+  setTimeout(() => {
+    behaviorSubject.next('data: ' + new Date().toLocaleTimeString());
+  }, 300);
+  return behaviorSubject;
+};
+
+let obp = new Observable(subscriber => {
+  setTimeout(() => {
+    subscriber.next('data: ' + new Date().toLocaleTimeString());
+    subscriber.complete();
+  }, 300);
+})
+
+let replaySubject = new ReplaySubject<string | null>(1);
+const callReplay = () => {
+  // obp.subscribe(replaySubject);
+  setTimeout(() => {
+    replaySubject.next('data: ' + new Date().toLocaleTimeString());
+  }, 300);
+  return replaySubject.pipe(skip(1));
+}
+const ajax = new Observable((subscriber) => {
+  setTimeout(() => {
+    subscriber.next('data: ' + new Date().toLocaleTimeString());
+  }, 300);
+});
+const ajax$ = new Subject();
+ajax.subscribe(ajax$);
+ajax$.subscribe((d) => {
+  console.log('[ajax]', 'global', d);
+});
+request().subscribe((d) => {
+  console.log('[request]', 'global', d);
+})
+callReplay().subscribe((d) => {
+  console.log('[replaySubject]', 'global', d);
+});
+fromEvent(document, 'click').subscribe(() => {
+  ajax$.subscribe((d) => {
+    console.log('[ajax]', 'click', d);
+  });
+  let s = request().subscribe((d) => {
+    console.log('[request]', 'click', d);
+  });
+  s.unsubscribe();
+  let s2 = callReplay().subscribe((d) => {
+    console.log('[replaySubject]', 'click', d);
+    // s2.unsubscribe();
+  });
+});
 
 let ob = of(123, 456);
 ob = from([234, 567]);

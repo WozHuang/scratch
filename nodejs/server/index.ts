fb305c985f142be5a46a267755645600b945dc02
WozHuang <wozhuang96@gmail.com>
2025-09-16 16:59:45 +0800

    koa 实现 sse 服务

--- nodejs/server/index.ts
+++ nodejs/server/index.ts
@@ -1,9 +1,11 @@
 import Koa from 'koa';
+import Router from '@koa/router';
 import { PassThrough } from 'stream';
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx) => {
+router.get('/chunked', async (ctx, next) => {
   // 设置响应头
   ctx.set('Transfer-Encoding', 'chunked');
   ctx.set('Content-Type', 'application/json');
@@ -15,7 +17,7 @@ app.use(async (ctx) => {
     stream = new PassThrough();
     ctx.body = stream;
   } else {
-    stream = ctx.res
+    stream = ctx.res;
   }
 
   // 模拟异步数据生成
@@ -23,10 +25,51 @@ app.use(async (ctx) => {
 
   for (const message of messages) {
     stream.write(JSON.stringify({ message, timestamp: Date.now() }) + '\n');
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
   }
 
   stream.end();
 });
 
-app.listen(3000);
+/**
+ * SSE 接口
+ * @see https://juejin.cn/post/7389081879909629993
+ */
+router.get('/sse', async (ctx, next) => {
+
+  ctx.set({
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+  });
+
+  ctx.status = 200;
+
+  const useStreamApi = false;
+  let stream;
+  if (useStreamApi) {
+    stream = new PassThrough();
+    ctx.body = stream;
+  } else {
+    stream = ctx.res;
+  }
+
+  // 模拟异步数据生成
+  const messages = ['First', 'Second', 'Third', 'Fourth', 'Fifth'];
+
+  for (const message of messages) {
+    stream.write(JSON.stringify({ message, timestamp: Date.now() }) + '\n');
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+
+  stream.end();
+
+  // 清理连接
+  ctx.req.on('close', () => {
+    stream.end();
+  });
+});
+
+app
+  .use(router.routes())
+  .listen(3000);
